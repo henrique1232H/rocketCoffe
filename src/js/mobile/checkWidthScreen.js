0d48984cda5586ea80b2bac98a870b1d1d34db20
henrique1232H <henriqueau111@gmail.com>
2024-01-17 19:04:28 -0300

    ajust some html tags

--- src/js/mobile/checkWidthScreen.js
+++ src/js/mobile/checkWidthScreen.js
@@ -3,5 +3,5 @@ export const checkWidthScreen = () => {
 
     const widthScreen = window.innerWidth;
     
-    widthScreen > 800 ? document.documentElement.classList.remove("mobile"): document.documentElement.classList.add("mobile")
+    widthScreen > 800 ? document.documentElement.classList.remove("mobile"): document.documentElement.classList.add("mobile");
 }
