258962b0ddc55095729853aaaa7ffeb36168cc88
henrique1232H <henriqueau111@gmail.com>
2024-01-19 20:30:18 -0300

    add server local and ajust some bugs css

--- src/js/mobile/checkWidthScreen.js
+++ src/js/mobile/checkWidthScreen.js
@@ -3,7 +3,7 @@ export const checkWidthScreen = () => {
 
     const widthScreen = window.innerWidth;
     
-    widthScreen > 900 ? document.documentElement.classList.remove("mobile"): document.documentElement.classList.add("mobile");
+    widthScreen > 950 ? document.documentElement.classList.remove("mobile"): document.documentElement.classList.add("mobile");
 
     document.querySelector(".Text").classList.remove("container")
 }
